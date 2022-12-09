5c08f7580e6a8ea53fa583f61b9acf4fe424c643
ridwan-bvk <readsmath@gmail.com>
2022-12-09 17:21:51 +0700

    sampai category

--- lib/page/category_page.dart
+++ lib/page/category_page.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -9,8 +11,88 @@ class CategoryPage extends StatefulWidget {
 }
 
 class _CategoryPageState extends State<CategoryPage> {
+  bool isExpense = false;
+  void OpenDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Center(
+                child: Column(
+              children: [Text("data")],
+              //sampai sini
+            )),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: SafeArea(
+          child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    // This bool value toggles the switch.
+                    Switch(
+                      value: isExpense,
+                      activeColor: Colors.green,
+                      activeTrackColor: Colors.green[200],
+                      inactiveThumbColor: Colors.red,
+                      onChanged: (bool value) {
+                        setState(() {
+                          isExpense = value;
+                        });
+                      },
+                    )
+                  ],
+                ),
+                IconButton(onPressed: () {}, icon: Icon(Icons.add))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Card(
+              elevation: 10,
+              child: ListTile(
+                leading: (isExpense)
+                    ? Icon(Icons.upload, color: Colors.green)
+                    : Icon(Icons.download, color: Colors.red),
+                title: Text('makan siang'),
+                trailing: Row(mainAxisSize: MainAxisSize.min, children: [
+                  IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+                  SizedBox(width: 10),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.delete))
+                ]),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Card(
+              elevation: 10,
+              child: ListTile(
+                leading: (isExpense)
+                    ? Icon(Icons.upload, color: Colors.green)
+                    : Icon(Icons.download, color: Colors.red),
+                title: Text('makan siang'),
+                trailing: Row(mainAxisSize: MainAxisSize.min, children: [
+                  IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+                  SizedBox(width: 10),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.delete))
+                ]),
+              ),
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
--- lib/page/main_page.dart
+++ lib/page/main_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:calendar_appbar/calendar_appbar.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -24,19 +25,33 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CalendarAppBar(
-        backButton: false,
-        accent: Colors.red,
-        locale: 'id',
-        onDateChanged: (value) => print(value),
-        firstDate: DateTime.now().subtract(Duration(days: 140)),
-        lastDate: DateTime.now(),
-      ),
+      appBar: (CurentIndex == 1)
+          ? PreferredSize(
+              preferredSize: const Size.fromHeight(100),
+              child: Container(
+                  child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 36, horizontal: 16),
+                child: Text("Categories",
+                    style: GoogleFonts.montserrat(fontSize: 20)),
+              )),
+            )
+          : CalendarAppBar(
+              backButton: false,
+              accent: Colors.red,
+              locale: 'id',
+              onDateChanged: (value) => print(value),
+              firstDate: DateTime.now().subtract(Duration(days: 140)),
+              lastDate: DateTime.now(),
+            ),
       body: _children[CurentIndex],
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: Colors.red,
-          child: Icon(Icons.add)),
+      floatingActionButton: Visibility(
+        visible: (CurentIndex == 0) ? true : false,
+        child: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.red,
+            child: Icon(Icons.add)),
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation
           .centerDocked, //biar sih floating nya ketengah
       bottomNavigationBar: BottomAppBar(
